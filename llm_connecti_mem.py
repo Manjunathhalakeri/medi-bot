21223a0d6bff31b637b4ae7cc6f072b0020f6ce5
Manjunathhalakeri <manjuhalakeri97@gmail.com>
2025-05-18 16:02:18 +0530

    Switch to OpenAI LLM and embeddings, update QA chain

--- llm_connecti_mem.py
+++ llm_connecti_mem.py
@@ -5,4 +5,67 @@ from langchain_core.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_core.document_loaders import TextLoader
+#from langchain_core.document_loaders import TextLoader
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv())
+
+
+# Step 1: Setup LLM (openai)
+
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+def load_llm():
+    llm = ChatOpenAI(
+        temperature=0.5,
+        model_name="gpt-3.5-turbo",  # or "gpt-4" if you have access
+        max_tokens=512
+    )
+    return llm
+
+CUSTOM_PROMPT_TEMPLATE = """
+Use the pieces of information provided in the context to answer user's question.
+If you dont know the answer, just say that you dont know, dont try to make up an answer. 
+Dont provide anything out of the given context
+
+Context: {context}
+Question: {question}
+
+Start the answer directly. No small talk please.
+"""
+
+def set_custom_prompt(custom_prompt_template):
+    prompt=PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+    return prompt
+
+# Load Database
+DB_FAISS_PATH = "vectorstore/db_faiss"
+embedding_model = OpenAIEmbeddings()
+db = FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+
+
+
+# Create QA chain
+# Load LLM
+llm = load_llm()
+
+# Create QA chain
+qa_chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=db.as_retriever(search_kwargs={'k':3}),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
+    )
+
+
+
+# Now invoke with a single query
+user_query=input("Write Query Here: ")
+response=qa_chain.invoke({'query': user_query})
+print("RESULT: ", response["result"])
+print("SOURCE DOCUMENTS: ", response["source_documents"])
+
+
